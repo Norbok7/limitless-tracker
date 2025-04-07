c22a3e04790ec79fc0710d70a82b5375df48a143
Garrett Duncan <duncansports_17@hotmail.com>
2025-04-07 10:59:51 -0500

    test initila lecture done

--- src/app/features/exercises/components/exercise-form/exercise-form.component.ts
+++ src/app/features/exercises/components/exercise-form/exercise-form.component.ts
@@ -1,4 +1,4 @@
-import { Component, inject } from '@angular/core';
+import { AfterViewInit, Component, ElementRef, inject, viewChild } from '@angular/core';
 import { ExerciseService } from '../../../../shared/services/exercise.service';
 import { FormsModule} from '@angular/forms';
 @Component({
@@ -7,11 +7,27 @@ import { FormsModule} from '@angular/forms';
   templateUrl: './exercise-form.component.html',
   styleUrl: './exercise-form.component.css'
 })
-export class ExerciseFormComponent {
+export class ExerciseFormComponent implements AfterViewInit {
   private exerciseService = inject(ExerciseService);
+
+  private form = viewChild.required<ElementRef<HTMLFormElement>>('form')
 
   name: string = '';
   duration: number = 0;
+
+    // TODO: use AfterViewInit
+    ngAfterViewInit() {
+
+      // logging the form element to ensure it is accessible
+      console.log('ngAfterViewInit called: Form Element Available!', this.form().nativeElement);
+
+      // adding an event listener to the form (if needed)
+      this.form().nativeElement.addEventListener('submit', (event) => {
+        console.log('Form submitted!' );
+      });
+
+  }
+
 
   addExercise() {
     this.exerciseService.addExercise(this.name, this.duration);
--- src/app/features/exercises/components/exercise-list/exercise-list.component.ts
+++ src/app/features/exercises/components/exercise-list/exercise-list.component.ts
@@ -1,7 +1,8 @@
-import { Component, inject } from '@angular/core';
+import { Component, inject, input } from '@angular/core';
 import { ExerciseService } from '../../../../shared/services/exercise.service';
 import { ExerciseFormComponent } from '../exercise-form/exercise-form.component';
 import { ExerciseItemComponent } from '../exercise-item/exercise-item.component';
+import { Exercise } from '../../../../shared/services/exercise-item.model';
 
 @Component({
   selector: 'app-exercise-list',
@@ -10,7 +11,10 @@ import { ExerciseItemComponent } from '../exercise-item/exercise-item.component'
   styleUrl: './exercise-list.component.css',
 })
 export class ExerciseListComponent {
+  exercise = input<Exercise>()
+  
   private exerciseService = inject(ExerciseService);
 
   exercises = this.exerciseService.getExercises();
+
 }
